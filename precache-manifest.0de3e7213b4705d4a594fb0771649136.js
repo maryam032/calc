34f5798534e838a2e2b940c5539b0e984e303839
maryam032 <maryamakram032@gmail.com>
2019-08-08 04:03:54 +0500

    Updates

---
+++ precache-manifest.0de3e7213b4705d4a594fb0771649136.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a72f60fbe35dbdfa056f5755d6a679",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "73ffbecf59fcb648a899",
+    "url": "/calc/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3b599f5c95ea6ec9e4bc",
+    "url": "/calc/static/js/2.91bbfc88.chunk.js"
+  },
+  {
+    "revision": "73ffbecf59fcb648a899",
+    "url": "/calc/static/js/main.21bac959.chunk.js"
+  },
+  {
+    "revision": "e60d74e24ba15745e6e3",
+    "url": "/calc/static/js/runtime~main.2a1b0bf0.js"
+  }
+]);
